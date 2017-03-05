537bff9c73ee416206aafbd7c72f64fb53bef6f7
lwb <1013485938@qq.com>
2017-03-05 17:22:22 +0800

    将数据改成网络获取

--- LWBVideoWeb/client/javascripts/sport/Sport.js
+++ LWBVideoWeb/client/javascripts/sport/Sport.js
@@ -1,17 +1,37 @@
 import React,{Component} from 'react';
-import Slider from '../components/Slider'
 import Search from '../components/Search'
 import Nav from '../components/Nav'
 import SportContent from './SportContent'
-import sportHeaderInfo from '../../data/sportHeaderInfo.json';
+import config from '../../data/config.json';
 
 class Sport extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sportHeaderInfo: null
+        }
+    }
+
+    componentDidMount(){
+        var self = this;
+        fetch(config.host+"/data/sportHeaderInfo.json")
+            .then((res)=>res.json())
+            .then((resJson)=>{
+                self.setState({
+                    sportHeaderInfo: resJson
+                });
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
+    }
 
     render(){
+        var sportHeaderInfo = this.state.sportHeaderInfo;
         return (
             <div>
-                <Search  info={sportHeaderInfo.data["search_content"]}/>
-                <Nav info={sportHeaderInfo.data["nav"]}/>
+                <Search  info={(sportHeaderInfo && sportHeaderInfo.data["search_content"])||[]}/>
+                <Nav info={(sportHeaderInfo && sportHeaderInfo.data["nav"])||[]}/>
                 <div style={{width:1280,display:'flex',justifyContent:'center',position:'absolute',top:120}}>
                     <SportContent />
                 </div>
