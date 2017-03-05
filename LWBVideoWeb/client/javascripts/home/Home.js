687739fdcee285e9d02603439a254337c31f4266
lwb <1013485938@qq.com>
2017-03-05 17:19:08 +0800

    将数据改成网络获取

--- LWBVideoWeb/client/javascripts/home/Home.js
+++ LWBVideoWeb/client/javascripts/home/Home.js
@@ -3,19 +3,37 @@ import Slider from '../components/Slider'
 import Search from '../components/Search'
 import Nav from '../components/Nav'
 import HomeContent from './HomeContent'
-import homeHeaderInfo from '../../data/homeHeaderInfo.json';
+import config from '../../data/config.json';
 
 class Home extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            homeHeaderInfo: null
+        }
+    }
+
+    componentDidMount(){
+        var self = this;
+        fetch(config.host+"/data/homeHeaderInfo.json")
+            .then((res)=>res.json())
+            .then((resJson)=>{
+                self.setState({
+                    homeHeaderInfo: resJson
+                });
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
     }
 
     render(){
+        var homeHeaderInfo = this.state.homeHeaderInfo;
         return (
-            <div>
-                <Slider info={homeHeaderInfo.data["background_slider"]} />
-                <Search info={homeHeaderInfo.data["search_content"]}/>
-                <Nav info={homeHeaderInfo.data["nav"]}/>
+            <div style={{display:'flex'}}>
+                <Slider info={(homeHeaderInfo && homeHeaderInfo.data["background_slider"])||[]} />
+                <Search info={(homeHeaderInfo && homeHeaderInfo.data["search_content"]) || ""}/>
+                <Nav info={(homeHeaderInfo && homeHeaderInfo.data["nav"]) || []}/>
                 <div style={{width:1280,display:'flex',justifyContent:'center',backgroundColor:'white',position:'absolute',zIndex:1,top:520}}>
                     <HomeContent />
                 </div>
