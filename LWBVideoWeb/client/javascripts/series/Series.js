68fa97c6951ba2434b042f2aaf29a5be4631423e
lwb <1013485938@qq.com>
2017-03-05 17:21:20 +0800

    将数据改成网络获取

--- LWBVideoWeb/client/javascripts/series/Series.js
+++ LWBVideoWeb/client/javascripts/series/Series.js
@@ -3,19 +3,37 @@ import Slider from '../components/Slider'
 import Search from '../components/Search'
 import Nav from '../components/Nav'
 import SeriesContent from './SeriesContent'
-import seriesHeaderInfo from '../../data/seriesHeaderInfo.json';
+import config from '../../data/config.json';
 
 class Series extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            seriesHeaderInfo: null
+        }
+    }
+
+    componentDidMount(){
+        var self = this;
+        fetch(config.host+"/data/seriesHeaderInfo.json")
+            .then((res)=>res.json())
+            .then((resJson)=>{
+                self.setState({
+                    seriesHeaderInfo: resJson
+                });
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
     }
 
     render(){
+        var seriesHeaderInfo = this.state.seriesHeaderInfo;
         return (
             <div>
-                <Slider info={seriesHeaderInfo.data["background_slider"]} />
-                <Search  info={seriesHeaderInfo.data["search_content"]}/>
-                <Nav info={seriesHeaderInfo.data["nav"]}/>
+                <Slider info={(seriesHeaderInfo && seriesHeaderInfo.data["background_slider"])||[]} />
+                <Search  info={(seriesHeaderInfo && seriesHeaderInfo.data["search_content"])||""}/>
+                <Nav info={(seriesHeaderInfo && seriesHeaderInfo.data["nav"])||[]}/>
                 <div style={{width:1280,display:'flex',justifyContent:'center',backgroundColor:'white',position:'absolute',zIndex:1,top:520}}>
                     <SeriesContent />
                 </div>
