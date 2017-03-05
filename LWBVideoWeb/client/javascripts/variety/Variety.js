bcf60f3c2d6d9117f1e7192b641613817691845b
lwb <1013485938@qq.com>
2017-03-05 17:23:29 +0800

    将数据改成网络获取

--- LWBVideoWeb/client/javascripts/variety/Variety.js
+++ LWBVideoWeb/client/javascripts/variety/Variety.js
@@ -3,19 +3,37 @@ import Slider from '../components/Slider'
 import Search from '../components/Search'
 import Nav from '../components/Nav'
 import VarietyContent from './VarietyContent'
-import varietyHeaderInfo from '../../data/varietyHeaderInfo.json';
+import config from '../../data/config.json';
 
 class Variety extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            varietyHeaderInfo: null
+        }
+    }
+
+    componentDidMount(){
+        var self = this;
+        fetch(config.host+"/data/varietyHeaderInfo.json")
+            .then((res)=>res.json())
+            .then((resJson)=>{
+                self.setState({
+                    varietyHeaderInfo: resJson
+                });
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
     }
 
     render(){
+        var varietyHeaderInfo = this.state.varietyHeaderInfo;
         return (
             <div>
-                <Slider info={varietyHeaderInfo.data["background_slider"]} />
-                <Search  info={varietyHeaderInfo.data["search_content"]}/>
-                <Nav info={varietyHeaderInfo.data["nav"]}/>
+                <Slider info={(varietyHeaderInfo && varietyHeaderInfo.data["background_slider"])||[]} />
+                <Search  info={(varietyHeaderInfo && varietyHeaderInfo.data["search_content"])||""}/>
+                <Nav info={(varietyHeaderInfo && varietyHeaderInfo.data["nav"])||[]}/>
                 <div style={{width:1280,display:'flex',justifyContent:'center',backgroundColor:'white',position:'absolute',zIndex:1,top:520}}>
                     <VarietyContent />
                 </div>
