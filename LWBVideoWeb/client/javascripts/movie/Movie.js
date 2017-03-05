be2911d55b6efe3fcc3c08d71ae68d27dcd7675a
lwb <1013485938@qq.com>
2017-03-05 17:20:14 +0800

    将数据改成网络获取

--- LWBVideoWeb/client/javascripts/movie/Movie.js
+++ LWBVideoWeb/client/javascripts/movie/Movie.js
@@ -3,19 +3,37 @@ import Slider from '../components/Slider'
 import Search from '../components/Search'
 import Nav from '../components/Nav'
 import MovieContent from './MovieContent'
-import movieHeaderInfo from '../../data/movieHeaderInfo.json';
+import config from '../../data/config.json';
 
 class Movie extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            movieHeaderInfo: null
+        }
+    }
+
+    componentDidMount(){
+        var self = this;
+        fetch(config.host+"/data/movieHeaderInfo.json")
+            .then((res)=>res.json())
+            .then((resJson)=>{
+                self.setState({
+                    movieHeaderInfo: resJson
+                });
+            })
+            .catch((err)=>{
+                console.log(err);
+            });
     }
 
     render(){
+        var movieHeaderInfo = this.state.movieHeaderInfo;
         return (
             <div>
-                <Slider info={movieHeaderInfo.data["background_slider"]} />
-                <Search  info={movieHeaderInfo.data["search_content"]}/>
-                <Nav info={movieHeaderInfo.data["nav"]}/>
+                <Slider info={(movieHeaderInfo && movieHeaderInfo.data["background_slider"]) || []} />
+                <Search  info={(movieHeaderInfo && movieHeaderInfo.data["search_content"]) || ""}/>
+                <Nav info={(movieHeaderInfo && movieHeaderInfo.data["nav"]) || []}/>
                 <div style={{width:1280,display:'flex',justifyContent:'center',backgroundColor:'white',position:'absolute',zIndex:1,top:520}}>
                     <MovieContent />
                 </div>
